7459c094e67e971d96467ebb779948664061feef
kenorb <kenorb@users.noreply.github.com>
2022-01-09 21:54:52 +0000

    Adds Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <iframe
+                src="https://explorer.functionx.io/fxcore/validator/fxvaloper1337eaaxxqd7l3jw6hwsrh0rvcjgfzgwqdtfq8x"
+                style={{ height: `100%`, width: `100%` }}
+                title="EA's Validator Details"
+            />
+        </div>
+    );
+  };
+  export default Footer;
