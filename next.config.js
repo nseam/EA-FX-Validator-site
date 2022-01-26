30ae036c9316f0b78c7aa8b3278665424959feff
kenorb <kenorb@users.noreply.github.com>
2022-01-26 21:01:01 +0000

    Updates links with base path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 const isProd = process.env.NODE_ENV === 'prod'
 module.exports = {
   assetPrefix: isProd ? process.env.BASE_PATH : '',
-  basePath: isProd ? process.env.BASE_PATH : '',
+  publicRuntimeConfig: {
+    basePath: isProd ? process.env.BASE_PATH : '',
+    staticFolder: '/static',
+  },
   distDir: process.env.BUILD_PATH,
 }
